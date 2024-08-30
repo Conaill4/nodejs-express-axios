ee8f6100867b2feb18ba33179ed63d7990016224
Conaill <conaill2004@outlook.com>
2024-08-29 23:18:15 -0700

    Branding

--- src/controllers/HomePageController.ts
+++ src/controllers/HomePageController.ts
@@ -1,5 +1,5 @@
 import express from "express";
 
 export const getHomePage = async (req: express.Request, res: express.Response): Promise<void> => {
-    res.render('homepage.html');
+    res.render('home.html');
 }
