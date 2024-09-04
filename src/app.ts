eb4744c4d5c107dd3dc4d84d0f1a4b18a7d3769e
Luke Devlin <ldevlin559@gmail.com>
2024-09-04 16:00:50 +0100

    Authorisation added

--- src/app.ts
+++ src/app.ts
@@ -8,6 +8,8 @@ import { dateFilter } from "./filters/dateFilter";
 import { getHomePage } from "./controllers/HomePageController";
 import { getLoginForm, postLoginForm, logoutForm} from "./controllers/AuthController";
 import { checkLoginStatus } from "./middleware/AuthStatus";
+import { allowRoles } from "./middleware/AuthMiddleware";
+import { UserRole } from "./models/JwtToken";
 
 const app = express();
 
@@ -37,7 +39,7 @@ declare module "express-session" {
   }
 }
  
-app.get('/job-roles', getJobsList);
+app.get('/job-roles', allowRoles([UserRole.Admin, UserRole.User]), getJobsList);
  
 app.listen(3000, () => {
     console.log('Server started on port 3000');
@@ -46,5 +48,5 @@ app.listen(3000, () => {
 app.get('/', getHomePage);
 app.get('/loginForm', getLoginForm);
 app.post('/loginForm', postLoginForm);
-app.post('/logout', logoutForm );
+app.post('/logout',  allowRoles([UserRole.Admin, UserRole.User]), logoutForm );
 
