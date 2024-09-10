c7379be576e907b2e9f1c9ba2407c0ada1b0f45a
JohnnyWork718 <johnny----18@hotmail.com>
2024-09-10 14:35:10 +0100

    added tests

--- test/ui/aws/AwsLoginUITests.ts
+++ test/ui/aws/AwsLoginUITests.ts
@@ -19,21 +19,21 @@ describe('Job Role Info Page Tests - AWS', () => {
     const login = async (driver: WebDriver, email: string, password: string) => {
         // Navigate to the login page
         await driver.get(url);
-    
+
         // Fill in the login form
         await driver.findElement(webdriver.By.id('email')).sendKeys(email);
         await driver.findElement(webdriver.By.id('password')).sendKeys(password);
-        
+
         // Click submit
         await driver.findElement(webdriver.By.id('submit')).click();
     };
 
     const logout = async (driver: WebDriver) => {
-        await driver.findElement(webdriver.By.css("button[type='submit']"));
+        await driver.findElement(webdriver.By.css("button[type='submit']")).click();
     }
 
-    describe('Returns login form', ()=>{
-        it('When /loginform endpoint is accessed, the login form should appear', async ()=>{
+    describe('Returns login form', () => {
+        it('When /loginform endpoint is accessed, the login form should appear', async () => {
             await driver.get(url);
             const loginForm = await driver.findElement(webdriver.By.className('login_form'));
             const isFormDisplayed = await loginForm.isDisplayed();
@@ -41,64 +41,75 @@ describe('Job Role Info Page Tests - AWS', () => {
         })
     })
 
-    describe('email and password inputs empty', ()=>{
-        it('When the login form is accessed the email input should be visible', async ()=>{
+    describe('email and password inputs empty', () => {
+        it('When the login form is accessed the email input should be visible', async () => {
             await driver.get(url);
             const emailInput = await driver.findElement(webdriver.By.id('email')).isDisplayed();
             expect(emailInput).to.be.true;
         })
 
-        it('When the login form is accessed the password input should be visible', async ()=>{
+        it('When the login form is accessed the password input should be visible', async () => {
             await driver.get(url);
             const passwordInput = await driver.findElement(webdriver.By.id('password')).isDisplayed();
             expect(passwordInput).to.be.true;
         })
     })
 
-    describe('Valid Admin details login', ()=>{
-        it('When valid admin details are entered the user should be directed to the homepage', async()=>{
+    describe('Valid Admin details login', () => {
+        it('When valid admin details are entered the user should be directed to the homepage', async () => {
             await login(driver, 'admin@kainos.com', 'Adm1n$');
             const newPage = await driver.getTitle();
             expect(newPage).to.equal('Home - Kainos');
         })
 
-        it('logout button should appear on homepage after login', async ()=>{
+        it('logout button should appear on homepage after login', async () => {
             const logoutButton = await driver.findElement(webdriver.By.css("button[type='submit']")).getText();
             expect(logoutButton).to.equal('Logout');
         })
     })
 
-    describe('Valid Applicant User details login', ()=>{
-        it('When valid Applicant user details are entered the user should be directed to the homepage', async()=>{
+    describe('Valid Applicant User details login', () => {
+        it('When valid Applicant user details are entered the user should be directed to the homepage', async () => {
             await login(driver, 'user@kainos.com', 'Us3r$');
             const newPage = await driver.getTitle();
             expect(newPage).to.equal('Home - Kainos');
         })
 
-        it('logout button should appear on homepage after login', async ()=>{
+        it('logout button should appear on homepage after login', async () => {
             const logoutButton = await driver.findElement(webdriver.By.css("button[type='submit']")).getText();
             await logout(driver);
             expect(logoutButton).to.equal('Logout');
         })
     })
 
-    describe('', ()=>{
-        it('', async ()=>{
-            
+
+    describe('Working logout button', ()=>{
+        it('When user is logged in and presses log out they should be directed back to the login page', async ()=>{
+            await login(driver, 'admin@kainos.com', 'Adm1n$');
+            const homepage = await driver.getTitle();
+            await logout(driver);
+            const loginPage = await driver.getTitle();
+            expect(homepage).to.equal('Home - Kainos');
+            expect(loginPage).to.equal('Login - Kainos');
         })
     })
 
-    
+    describe('invalid login details', ()=>{
 
-    // describe('Working logout button', ()=>{
-    //     it('When user is logged in and presses log out they should be directed back to the login page', async ()=>{
-    //         await login(driver, 'admin@kainos.com', 'Adm1n$');
-    //         const homepage = await driver.getTitle();
-    //         await logout(driver);
-    //         const loginPage = await driver.getTitle();
-    //         expect(homepage).to.equal('Home - Kainos');
-    //         expect(loginPage).to.equal('Login - Kainos');
-    //     })
-    // })
+        it('When properly formatted but incorrect details are entered, the user should stay on login page', async()=>{
+            await login(driver, 'incorrectDetails@kainos.com', 'Incorr3ct$');
+            const currentPage = await driver.getTitle();
+            expect(currentPage).to.equal('Login - Kainos');
+        })
+        
+        it('When properly formatted but incorrect details are entered, the user should see an error message', async()=>{
+            await login(driver, 'incorrectDetails@kainos.com', 'Incorr3ct$');
+            const message = await driver.findElement(webdriver.By.css("div[class='input_box'] h3")).getText();
+            expect(message).to.equal('Failed to get user.');
+        })
+
+    })
+
+
 
 })
